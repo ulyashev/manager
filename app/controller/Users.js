bfb881d8f70c06568ed4ec3f37792cfcd24f13d4
ulyashev <alexeyulyashev@gmail.com>
2019-06-03 12:56:32 +0300

    Add method 'userAdd'

--- app/controller/Users.js
+++ app/controller/Users.js
@@ -4,12 +4,16 @@ Ext.define('AM.controller.Users', {
     models: ['User'],
     views: [
         'user.List',
-        'user.Edit'
+        'user.Edit',
+        'user.Add'
     ],
     init: function() {
         this.control({
             'viewport > userlist': {
                 itemdblclick: this.editUser
+            },
+            'button': {
+                click: this.addUser
             },
             'useredit button[action=save]': {
                 click: this.updateUser
@@ -25,6 +29,11 @@ Ext.define('AM.controller.Users', {
         record.set(values);
         this.getUsersStore().sync();
         win.close();
+    },
+    addUser: function() {
+        var view = Ext.widget('useradd');
+
+        view.down('form').loadRecord(record);
     },
     editUser: function(grid, record) {
         var view = Ext.widget('useredit');
---
+++ app/view/user/Add.js
@@ -0,0 +1,42 @@
+Ext.define('AM.view.user.Add', {
+    extend: 'Ext.window.Window',
+    alias: 'widget.useradd',
+
+    title: 'Add User',
+    layout: 'fit',
+    autoShow: true,
+
+    initComponent: function() {
+        this.items = [
+            {
+                xtype: 'form',
+                items: [
+                    {
+                        xtype: 'textfield',
+                        name : 'name',
+                        fieldLabel: 'Name'
+                    },
+                    {
+                        xtype: 'textfield',
+                        name : 'email',
+                        fieldLabel: 'Email'
+                    }
+                ]
+            }
+        ];
+
+        this.buttons = [
+            {
+                text: 'Save',
+                action: 'save'
+            },
+            {
+                text: 'Cancel',
+                scope: this,
+                handler: this.close
+            }
+        ];
+
+        this.callParent(arguments);
+    }
+});
